2b68abe47dd4c1f0c7ee81498b2f697300f9a5a1
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-11-23 16:05:53 -0800

    initial configuration variable design

---
+++ erosion/config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import common
+
+from strata import Variable, LayerSet, ConfigSpec
+
+"""
+Configuration variables:
+
+* Link database file path
+* Enable/disable local file hosting
+* Local file hosting root path
+* Server host
+* Server port
+* Full host URL
+* Secret key (for cookie signing)
+"""
+
+"""
+Origins:
+
+* Command line arguments
+* Environment variables
+* Config file
+* code/defaults/Config kwargs
+"""
