0936bb422c93a168cde6c70fe263a959e279dbab
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-12-01 17:34:14 -0800

    playing with configuration layers

--- erosion/config.py
+++ erosion/config.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
+import json
+
 import common
 
-from strata import Variable, Layer, LayerSet, ConfigSpec
+from strata import Variable, Layer, LayerSet, ConfigSpec, Provider
 from strata.layers import CLILayer, KwargLayer, EnvVarLayer
+from strata.errors import MissingValue, NotProvidable
 
 """
 Configuration variables:
@@ -45,6 +49,7 @@ class ServerHost(Variable):
 class ServerPort(Variable):
     cli_arg_name = 'port'
     is_config_kwarg = True
+    json_config_key = 'port'
 
 
 class SecretKey(Variable):
@@ -55,12 +60,63 @@ VAR_LIST = [LinkDatabasePath, LocalHostingRootPath,
             ServerHost, ServerPort, SecretKey]
 
 
+class ProjectJSONConfigLayer(Layer):
+    _autoprovided = ['project_json_config']
+
+    def project_json_config(self):
+        try:
+            fh = open('./config.json')
+        except IOError:
+            raise MissingValue()
+        return json.load(fh)
+
+    @classmethod
+    def _get_provider(cls, var):
+        try:
+            return super(ProjectJSONConfigLayer, cls)._get_provider(var)
+        except NotProvidable as npe:
+            pass
+        json_config_key = getattr(var, 'json_config_key', None)
+        if not json_config_key:
+            raise npe
+
+        def _get_project_json_value(project_json_config):
+            return project_json_config[json_config_key]
+        return Provider(cls, var.name, _get_project_json_value)
+
+
+class UserJSONConfigLayer(Layer):
+    _autoprovided = ['user_json_config']
+
+    def user_json_config(self):
+        try:
+            fh = open(os.path.expanduser('~/.erosion_config'))
+        except IOError:
+            raise MissingValue()
+        return json.load(fh)
+
+    @classmethod
+    def _get_provider(cls, var):
+        try:
+            return super(UserJSONConfigLayer, cls)._get_provider(var)
+        except NotProvidable as npe:
+            pass
+        json_config_key = getattr(var, 'json_config_key', None)
+        if not json_config_key:
+            raise npe
+
+        def _get_user_json_value(user_json_config):
+            return user_json_config[json_config_key]
+        return Provider(cls, var.name, _get_user_json_value)
+
+
 class DevDefaultLayer(Layer):
     def secret_key(self):
         return 'configmanagementisimportantandhistoricallyhard'
 
 
-_COMMON_LAYERS = [KwargLayer, CLILayer, EnvVarLayer]
+_COMMON_LAYERS = [KwargLayer, CLILayer, EnvVarLayer,
+                  ProjectJSONConfigLayer, UserJSONConfigLayer]
 _PROD_LAYERSET = LayerSet('prod', _COMMON_LAYERS)
 PROD_CONFIGSPEC = ConfigSpec(VAR_LIST, _PROD_LAYERSET)
 
